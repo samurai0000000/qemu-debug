89f3772ca8a92d62978bcde230f9eeeeec735c0c
Charles Chiou <carlos@viosoft.com.tw>
2014-11-05 03:38:25 +0800

    Figured out how to make single project.
    Signed-off-by: Charles Chiou <carlos@viosoft.com.tw>

--- mk/main.mk
+++ mk/main.mk
@@ -1,6 +1,12 @@
 .PHONY: qemu
 
 qemu: out/host/linux-x86/bin/acp
+	cd external/qemu && ONE_SHOT_MAKEFILE=external/qemu/Android.mk $(MAKE) -C $(TOP) -f build/core/main.mk all_modules
+
+.PHONY: clean
+
+clean:
+	rm -rf out
 
 out/host/linux-x86/bin/acp: qemu-debug/bin/acp
-	mkdir -p out/host/linux-x86/bin &&bin cp $< $@
+	mkdir -p out/host/linux-x86/bin && cp $< $@
